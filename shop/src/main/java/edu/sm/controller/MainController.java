9d700cc55564bb73d09fad6e6c0c20b45f232ab5
jmlee <jmlee@tonesol.com>
2025-09-03 16:57:24 +0900

    0902

--- shop/src/main/java/edu/sm/controller/MainController.java
+++ shop/src/main/java/edu/sm/controller/MainController.java
@@ -1,6 +1,7 @@
 package edu.sm.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @Slf4j
 public class MainController {
+
+
+
     @RequestMapping("/")
     public String main(Model model) {
-        log.info("Start Main ,,,,,,");
         // Database 데이터를 가지고 온다.
         return "index";
     }
