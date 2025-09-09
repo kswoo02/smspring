918d794f605d87cd32ec886f0e13dd6136ddad67
jmlee <jmlee@tonesol.com>
2025-09-09 13:21:56 +0900

    0909

--- shop/src/main/java/edu/sm/controller/ProductController.java
+++ shop/src/main/java/edu/sm/controller/ProductController.java
@@ -1,14 +1,21 @@
 package edu.sm.controller;
 
+import edu.sm.app.dto.Product;
+import edu.sm.app.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @Slf4j
 @RequestMapping("/product")
+@RequiredArgsConstructor
 public class ProductController {
+    final ProductService productService;
 
     String dir="product/";
 
@@ -25,9 +32,19 @@ public class ProductController {
         return "index";
     }
     @RequestMapping("/get")
-    public String get(Model model) {
-        model.addAttribute("center",dir+"get");
-        model.addAttribute("left",dir+"left");
+    public String get(Model model) throws Exception {
+        List<Product> list = null;
+
+        list = productService.get();
+        model.addAttribute("plist", list);
+        model.addAttribute("left", dir+"left");
+        model.addAttribute("center", dir+"get");
         return "index";
     }
+    @RequestMapping("/registerimpl")
+    public String registerimpl(Model model, Product product) throws Exception {
+        productService.register(product);
+        return "redirect:/product/get";
+    }
+
 }
